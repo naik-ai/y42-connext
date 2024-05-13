95d394b67110ec0fb2af0f9d323364f0c648f010
Jay Naik <jay@naik.ai>
2024-05-13 19:07:41 +0530

    update broken ttr ttv public transfer table

--- src/models/Deduplication/transfers_with_price_ttr_ttv_y42_dedup.sql
+++ src/models/Deduplication/transfers_with_price_ttr_ttv_y42_dedup.sql
@@ -1,7 +1,5 @@
 SELECT
-     *,
-     tf.execute_timestamp - tf.xcall_timestamp AS ttv,
-     tf.reconcile_timestamp - tf.xcall_timestamp AS ttr,
+     *
      row_number() OVER (PARTITION BY `transfer_id` ORDER BY tf.update_time DESC) AS row_num
-FROM {{ source('Cartographer', 'public_transfers_with_price') }} AS tf
+FROM {{ source('Cartographer', 'public_transfers_with_price_ttr_ttv') }} AS tf
 WHERE TRUE QUALIFY row_num = 1
