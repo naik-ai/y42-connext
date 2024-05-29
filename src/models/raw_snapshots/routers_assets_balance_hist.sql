eb8b065bb96550dfa877330bb42167ce0fa3d4a9
Jay Naik <jay@naik.ai>
2024-05-29 16:34:27 +0530

    snapshot model changes

--- src/models/raw_snapshots/routers_assets_balance_hist.sql
+++ src/models/raw_snapshots/routers_assets_balance_hist.sql
@@ -2,16 +2,13 @@
 
 WITH source_data AS (
     SELECT
-        pool_id,
-        other_columns,
+        *,
         CURRENT_TIMESTAMP() AS snapshot_time  -- Captures the current UTC timestamp
     FROM {{ source('Cartographer', 'public_asset_balances') }}
 )
 
 SELECT
-    *,
-    snapshot_time,
-    pool_id
+    *
 FROM source_data
 {% if is_incremental() %}
 {% endif %}
