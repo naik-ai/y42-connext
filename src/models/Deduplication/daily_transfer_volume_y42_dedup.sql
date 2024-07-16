f068749f05ee87ecf386ffc15320990c2c0891b5
Jay Naik <jay@naik.ai>
2024-07-17 00:35:25 +0530

    removed deduped data files

--- src/models/Deduplication/daily_transfer_volume_y42_dedup.sql
+++
@@ -1,5 +0,0 @@
--- SELECT
---     *,
-    -- row_number() OVER (PARTITION BY `id` ORDER BY `volume` DESC) AS ROW_NUM
--- FROM {{ source('Cartographer', 'public_daily_transfer_volume') }}
--- WHERE TRUE QUALIFY ROW_NUM = 1
--- src/models/Deduplication/transfers_with_price_ttr_ttv_y42_dedup.sql
+++
@@ -1,5 +0,0 @@
--- SELECT
---      *,
-     -- row_number() OVER (PARTITION BY `transfer_id` ORDER BY tf.update_time DESC) AS row_num
--- FROM {{ source('Cartographer', 'public_transfers_with_price_ttr_ttv') }} AS tf
--- WHERE TRUE QUALIFY row_num = 1
