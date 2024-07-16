02c9e35c5898e8947d9ad67a7650f259d7924789
Jay Naik <jay@naik.ai>
2024-07-17 00:33:54 +0530

    removed unused data

--- src/models/Deduplication/daily_transfer_volume_y42_dedup.sql
+++ src/models/Deduplication/daily_transfer_volume_y42_dedup.sql
@@ -1,5 +1,5 @@
-SELECT
-    *,
-    row_number() OVER (PARTITION BY `id` ORDER BY `volume` DESC) AS ROW_NUM
-FROM {{ source('Cartographer', 'public_daily_transfer_volume') }}
-WHERE TRUE QUALIFY ROW_NUM = 1
+-- SELECT
+--     *,
+    -- row_number() OVER (PARTITION BY `id` ORDER BY `volume` DESC) AS ROW_NUM
+-- FROM {{ source('Cartographer', 'public_daily_transfer_volume') }}
+-- WHERE TRUE QUALIFY ROW_NUM = 1
